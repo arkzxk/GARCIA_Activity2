d680381b183c8f0976a857742b9a97f895d63c4b
arkzxk <dggarcia827@gmail.com>
2026-01-08 02:37:08 +0800

    Commit for added line

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace Activity1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello! My name is Duncan.");
+            Console.WriteLine("But you can also call me Dan.");
         }
     }
 }

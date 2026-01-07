f5b2701711e0613f1ccf1303c4425934d49f4434
arkzxk <dggarcia827@gmail.com>
2026-01-08 02:43:27 +0800

    Reverted back to previous version

--- Program.cs
+++ Program.cs
@@ -7,8 +7,6 @@ namespace Activity1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello! My name is Duncan.");
-            Console.WriteLine("But you can also call me Dan.");
-            Console.WriteLine("I am 20 years old, turning 21 on August.");
         }
     }
 }
